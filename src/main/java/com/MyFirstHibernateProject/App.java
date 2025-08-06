0456ba73310054fdfc7624d35a25d13befa68116
MUHAMMED FAWZAN A <85784499+fawzanoachira@users.noreply.github.com>
2025-08-06 10:10:02 +0530

    Update App.java

--- src/main/java/com/MyFirstHibernateProject/App.java
+++ src/main/java/com/MyFirstHibernateProject/App.java
@@ -10,17 +10,22 @@ import com.MyFirstHibernateProject.entity.Employee;
 public class App {
 	public static void main(String[] args) {
 		try {
+			// For creating data into database
 			Configuration config = new Configuration();
 			config.configure();
 			SessionFactory sessionFactory = config.buildSessionFactory();
 			Session session = sessionFactory.openSession();
+
 			Employee employee = new Employee();
+
 			employee.setEmpId(1);
 			employee.setEmpName("fawzan");
 			employee.setEmpSalary(56000.0);
+
 			session.persist(employee);
 			Transaction beginTransaction = session.beginTransaction();
 			beginTransaction.commit();
+			
 			session.close();
 			sessionFactory.close();
 		} catch (Exception e) {
